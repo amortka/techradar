e639c24cb0abdcfb6d3ec312caaeda27500ec500
Adam Mortka <a.mortka@gmail.com>
2017-04-06 23:09:04 +0200

    solved tags around area name with multipart path

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import './app.scss';
 // const URL = 'https://technology-radar.firebaseio.com/snapshots/PGSNET/august-2016.json';
 const URL = 'https://technology-radar.firebaseio.com/snapshots/pgs-frontend/march-2017.json';
 
-const AREA_COLORS = ['#2A9D8F', '#E9C46A', '#F4A261', '#E76F51'];
+// const AREA_COLORS = ['#2A9D8F', '#E9C46A', '#F4A261', '#E76F51'];
+const AREA_COLORS = ['#5BC0EB', '#7FB800', '#FFB30F', '#FD151B'];
 const PI = 3.14159;
 
 function drawChart(data) {
@@ -16,7 +17,9 @@ function drawChart(data) {
     let g = svg.append('g')
         .attr('transform', `translate(${config.center.x}, ${config.center.y})`);
 
-    drawDebugLayer(g, config);
+    // drawDebugLayer(g, config);
+
+    drawItemsLines(g, data.items, config);
     drawItemLabels(g, data.items, config);
     drawAreaLebels(g, data, config);
 }
@@ -73,8 +76,7 @@ function drawDebugLayer(selection, config) {
         .attr('stroke-width', 2)
         .attr('d', lineFn);
 
-
-    debugGroup
+    /*debugGroup
         .append('g')
         .attr('class', 'debugLines')
         .selectAll('path')
@@ -84,7 +86,7 @@ function drawDebugLayer(selection, config) {
         .attr('fill', 'none')
         .attr('stroke', 'red')
         .attr('stroke-width', 0.5)
-        .attr('d', lineFn);
+        .attr('d', lineFn);*/
 
 }
 
@@ -128,19 +130,9 @@ function drawAreaLebels(selection, data, config) {
             name: area
         }))
         .map((area, idx, arr) => {
-            // console.log('area', area);
-            // console.log('idx', idx);
-            // console.log('idx', idx < arr.length-1 ? 1 : 0);
-
-            //area.startAngle
-
             area.startAngle = config.scaleRadialPosition(arr.reduce((total, curr, idxArea) => {
                 return idxArea < idx ? total + (curr.count) : total;
             }, 0));
-
-            // area.endAngle = config.scaleRadialPosition(arr.reduce((total, curr, idxArea) => {
-            //         return idxArea < idx ? total + curr.count : total;
-            //     }, idx > 0 ? -1 : 0) + area.count);
 
             area.endAngle = config.scaleRadialPosition(arr.reduce((total, curr, idxArea) => {
                 return idxArea < idx ? total + curr.count : total;
@@ -150,24 +142,167 @@ function drawAreaLebels(selection, data, config) {
         });
 
     let arc = d3.arc()
-        .innerRadius(config.radiusMax - 20)
-        .outerRadius(config.radiusMax - 23)
+        .innerRadius(config.radiusMax - 23)
+        .outerRadius(config.radiusMax - 20)
         .startAngle(d => d.startAngle)
         .endAngle(d => d.endAngle)
         // .padAngle(deg2rad(10));
 
-    group.selectAll('.areaFill')
+    group.selectAll('.areaLine')
         .data(areasData)
         .enter()
         .append('path')
-        .attr('class', 'areaFill')
+        .attr('class', 'areaLine')
         .attr('fill', d => d.color)
         .attr('d', arc)
         // .transition()
         // .duration(750)
         //.attrTween('d', arcTween(arc));
+/*
+    let arcLabelTag = d3.arc()
+        .innerRadius(config.radiusMax - 10)
+        .outerRadius(config.radiusMax - 30)
+        .startAngle(d => d.startAngle)
+        .endAngle(d => d.endAngle)
 
+      group.selectAll('.areaLabelTag')
+          .data(areasData)
+          .enter()
+          .append('path')
+          .attr('class', 'areaLabelTag')
+          .attr('fill', d => d.color)
+          .attr('d', arcLabel)*/
 
+      let labelRadius = config.radiusMax - 21.5;
+
+            group
+              .selectAll('.areaLabelArc')
+              .data(areasData)
+              .enter()
+              .append('path')
+              .attr('id', (d, idx) => 'areaLabelPath_' + idx)
+              .attr('class', 'areaLabelArc')
+              .attr('d', (d, idx) => {
+                let pStart = [labelRadius * Math.cos(d.startAngle - config.baseAngle), labelRadius * Math.sin(d.startAngle - config.baseAngle)];
+                let pEnd = [labelRadius * Math.cos(d.endAngle - config.baseAngle), labelRadius * Math.sin(d.endAngle - config.baseAngle)];
+
+                return `M ${pStart[0]},${pStart[1]} A ${labelRadius},${labelRadius} 0 0 1 ${pEnd[0]},${pEnd[1]}`;
+              });
+
+            group.selectAll('.areaLabelDebug')
+                .data(areasData)
+                .enter()
+                .append('text')
+                .attr('class', (d, idx) => 'areaLabelDebug ' + '#areaLabel_' + idx)
+                .attr('x', 0)
+                .attr('dy', 5)
+                .attr('text-anchor', 'middle')
+                .append('textPath')
+                .attr('xlink:href', (d, idx) => '#areaLabelPath_' + idx)
+                .attr('startOffset', '50%')
+                .text((d) => d.name);
+
+      let labelTagRadiusMax = config.radiusMax - 21.5 + 15;
+      //21.5
+      let labelTagRadiusMin = config.radiusMax - 21.5 - 15;
+
+      let tagsData = d3.selectAll('.areaLabelDebug').nodes()
+        .map((node, idx) => {
+          let textLength = node.getComputedTextLength();
+          let midAngle = (areasData[idx].startAngle + areasData[idx].endAngle) / 2;
+          let startAngle = areasData[idx].startAngle;
+          let endAngle = areasData[idx].endAngle;
+          let circumference = 2 * PI * labelRadius;
+
+          let offsetAngle = (textLength/2/circumference) * 2 * PI;
+          let offsetSide = 0.03;
+          let color = areasData[idx].color;
+
+          return {
+            color,
+            startAngle,
+            endAngle,
+            midAngle,
+            offsetAngle,
+            offsetSide
+          };
+        });
+
+        group
+          .selectAll('.areaLabelTag')
+          .data(tagsData)
+          .enter()
+          .insert('path')
+          .attr('fill', '#FFFFFF')
+          .attr('stroke', d => d.color)
+          .attr('stroke-width', 3)
+          .attr('class', 'areaLabelTag')
+          .attr('d', (d, idx) => {
+            let p1 = [labelTagRadiusMax * Math.cos(d.midAngle - config.baseAngle - d.offsetAngle), labelTagRadiusMax * Math.sin(d.midAngle - config.baseAngle - d.offsetAngle)];
+            let p2 = [labelTagRadiusMax * Math.cos(d.midAngle - config.baseAngle + d.offsetAngle), labelTagRadiusMax * Math.sin(d.midAngle - config.baseAngle + d.offsetAngle)];
+            let p3 = [labelRadius * Math.cos(d.midAngle - config.baseAngle + d.offsetAngle + d.offsetSide), labelRadius * Math.sin(d.midAngle - config.baseAngle + d.offsetAngle + d.offsetSide)];
+            let p4 = [labelTagRadiusMin * Math.cos(d.midAngle - config.baseAngle + d.offsetAngle), labelTagRadiusMin * Math.sin(d.midAngle - config.baseAngle + d.offsetAngle)];
+            let p5 = [labelTagRadiusMin * Math.cos(d.midAngle - config.baseAngle - d.offsetAngle), labelTagRadiusMin * Math.sin(d.midAngle - config.baseAngle - d.offsetAngle)];
+            let p6 = [labelRadius * Math.cos(d.midAngle - config.baseAngle - d.offsetAngle - d.offsetSide), labelRadius * Math.sin(d.midAngle - config.baseAngle - d.offsetAngle - d.offsetSide)];
+
+            return `
+              M ${p1[0]},${p1[1]}
+              A ${labelTagRadiusMax},${labelTagRadiusMax} 0 0 1 ${p2[0]},${p2[1]}
+              L ${p3[0]},${p3[1]}
+              L ${p4[0]},${p4[1]}
+              A ${labelTagRadiusMin},${labelTagRadiusMin} 0 0 0 ${p5[0]},${p5[1]}
+              L ${p6[0]},${p6[1]}
+              L ${p1[0]},${p1[1]}
+            `;
+          });
+
+          group.selectAll('.areaLabelDebug').remove();
+
+          group.selectAll('.areaLabel')
+              .data(areasData)
+              .enter()
+              .append('text')
+              .attr('class', (d, idx) => 'areaLabel ' + '#areaLabel_' + idx)
+              .attr('x', 0)
+              .attr('dy', 5)
+              .attr('text-anchor', 'middle')
+              .append('textPath')
+              .attr('xlink:href', (d, idx) => '#areaLabelPath_' + idx)
+              .attr('startOffset', '50%')
+              .text((d) => d.name);
+
+      console.log(JSON.stringify(tagsData, false, 2));
+      //textElement.node().getComputedTextLength();
+
+}
+
+function drawItemsLines(selection, items, config) {
+
+  let group = selection.select('g.itemsLines').empty() ? selection.append('g') : selection.select('g.itemsLines');
+
+  let linesData = items
+    .map(p => {
+      let angle = config.scaleRadialPositionWithBaseShift(p._pos);
+      return [
+        [config.radiusMin * Math.cos(angle), config.radiusMin * Math.sin(angle)],
+        [config.radiusMaxLine * Math.cos(angle), config.radiusMaxLine * Math.sin(angle)],
+      ]
+    })
+
+  let lineFn = d3.line()
+      .x(d => d[0])
+      .y(d => d[1]);
+
+  group
+      .selectAll('.itemLine')
+      .data(linesData)
+      .enter()
+      .append('path')
+      .attr('class', 'itemLine')
+      .attr('fill', 'none')
+      .attr('stroke', config.colors.lineLight)
+      .attr('stroke-width', 0.5)
+      .attr('d', lineFn);
 }
 
 // --- data functions
@@ -248,6 +383,10 @@ function getConfig(containerEl, items) {
         y: 500
     };
 
+    const colors = {
+      lineLight: '#c2c2c2'
+    }
+
     // --- estimate length of longest technology
     const longestLabel = items
         .map(item => item.name)
@@ -257,6 +396,9 @@ function getConfig(containerEl, items) {
     const longestLabelWidth = getTextLength(longestLabel, containerEl);
 
     const radiusMax = 0.5 * 0.98 * (bound - longestLabelWidth);
+    const radiusMaxLine = radiusMax - 33;
+
+    const radiusMin = 150;
 
     // --- angles calculcation with reserved space
     const angleStart = deg2rad(legendReserverAngle);
@@ -273,8 +415,12 @@ function getConfig(containerEl, items) {
         .domain([0, items.length]);
 
     return {
+        baseAngle,
+        colors,
         count,
         radiusMax,
+        radiusMaxLine,
+        radiusMin,
         center,
         scaleRadialPosition,
         scaleRadialPositionWithBaseShift,
